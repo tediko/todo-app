ef8b4cb78f2b8b232a2ca8076c782b22ee96ca7f
tediko <tediko123@wp.pl>
2021-04-08 14:18:57 +0200

    Rework toast to class

--- js/toast.js
+++ js/toast.js
@@ -1,25 +1,38 @@
-export const Toast = {
-    init() {
-        this.hideTimeout = null;
+export default class Toast {
+    constructor() {
+        if (!this.vars()) return false;
+        this.setupEvents();
+    }
 
-        this.el = document.createElement('div');
-        this.el.className = "toast";
-        this.el.setAttribute('data-toast', 'true');
-        document.body.appendChild(this.el);
-    },
+    vars() {
+        this.hideTimeout = null;
+        return true;
+    }
+
+    setupEvents() {
+        this.createToastElement();
+        this.toastEl.addEventListener('click', () => this.clear());
+    }
+
+    createToastElement() {
+        this.toastEl = document.createElement('div');
+        this.toastEl.className = "toast";
+        this.toastEl.setAttribute('data-toast', 'true');
+        document.body.appendChild(this.toastEl);
+    }
 
     show(message, state) {
         clearTimeout(this.hideTimeout);
 
-        this.el.className = `toast toast--visible toast--${state}`;
-        this.el.innerHTML = `<i class="i-${state} toast__icon"></i> ${message}`;
+        this.toastEl.className = `toast toast--visible toast--${state}`;
+        this.toastEl.innerHTML = `<i class="i-${state} toast__icon"></i> ${message}`;
 
         this.hideTimeout = setTimeout(() => {
-            this.el.classList.remove('toast--visible');
+            this.toastEl.classList.remove('toast--visible');
         }, 2000);
-    },
+    }
 
     clear() {
-        this.el.classList.remove('toast--visible');
+        this.toastEl.classList.remove('toast--visible');
     }
-};
+}
