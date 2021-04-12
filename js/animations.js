b73fcb6acf42d413d8099343d14304ea7e3afd7b
tediko <tediko123@wp.pl>
2021-04-12 16:34:53 +0200

    Added new animations

--- js/animations.js
+++ js/animations.js
@@ -25,15 +25,43 @@ let animations = {
             opacity: 0,
             height: 0
         }
-    ]
+    ],
+    toastIn: [
+        {
+            transform: 'translateY(20px)',
+            opacity: 0,
+        },
+        {
+            opacity: 0
+        },
+        {
+            transform: 'translateY(0)',
+            opacity: 1,
+            visibility: 'visible'
+        }
+    ],
+    toastOut: [
+        {
+            transform: 'translateX(0)',
+            opacity: 1,
+            visibility: 'visible'
+        },
+        {
+            transform: 'translateX(-50px)',
+            opacity: 0,
+            visibility: 'hidden'
+        }
+    ],
 };
 
 let fadeIn = animations.fadeIn;
 let fadeOut = animations.fadeOut;
+let toastIn = animations.toastIn;
+let toastOut = animations.toastOut;
 
 // Fn to add animation to element with options
 const addAnimation = (element, animation, options) => {
     return element.animate(animation, options);
 }
 
-export { fadeIn as animationFadeIn, fadeOut as animationFadeOut, addAnimation};
+export { fadeIn as animationFadeIn, fadeOut as animationFadeOut, toastIn as animationToastIn, toastOut as animationToastOut, addAnimation};
